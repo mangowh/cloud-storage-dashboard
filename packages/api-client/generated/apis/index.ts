847da5be7089f978a49ce01e22e7099c2e2f1fe3
Stefano Tambellini <s.tambellini@icloud.com>
2025-07-13 11:57:41 +0200

    feat: add api-client package

---
+++ packages/api-client/generated/apis/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthApi';
+export * from './HelloApi';
+export * from './S3Api';
+export * from './UsersApi';
---
+++ packages/api-client/generated/models/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CollectionOfObjectDto';
+export * from './CollectionOfUserResponseDto';
+export * from './CreateUserDto';
+export * from './LoginResponseDto';
+export * from './MessageDto';
+export * from './ObjectDto';
+export * from './ProfileDto';
+export * from './SignInDto';
+export * from './UserResponseDto';
